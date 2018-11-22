165c70174860b7cbf10f0dd60be45b7d06eb3189
Thomas JGENTI <thomas.jgenti@tech-advantage.com>
2018-11-22 11:43:45 +0100

    ag-grid update and format

--- src/App.js
+++ src/App.js
@@ -1,72 +1,119 @@
-import React, { Component } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { Component } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 
-import { Button } from "react-bootstrap";
+import { Button } from 'react-bootstrap'
+
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: stretch;
+`
+
+const AppHeader = styled.div`
+  width: 100%;
+`
+
+const AppFooter = styled.div`
+  width: 100%;
+  padding: 5px;
+  background-color: whitesmoke;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FooterRight = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const AppBody = styled.div`
+  flex-grow: 1;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  text-align: left;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: stretch;
+`
 
 class SquareRenderer extends Component {
   render() {
-    const { value } = this.props;
+    const { value } = this.props
     return (
       <Button bsStyle="primary" bsSize="xs">
         {value}
       </Button>
-    );
+    )
   }
 }
 
-const carMakers = [
-  "Ford",
-  "Toyota",
-  "Citroën",
-  "Mercedes",
-  "Peugeot",
-  "Skoda",
-  "Vauxhall"
-];
+const carMakers = ['Ford', 'Toyota', 'Citroën', 'Mercedes', 'Peugeot', 'Skoda', 'Vauxhall']
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       columnDefs: [
-        { headerName: "Make", field: "make" },
-        { headerName: "Model", field: "model", editable: true },
+        { headerName: 'Make', field: 'make' },
+        { headerName: 'Model', field: 'model', editable: true },
         {
-          headerName: "Price",
-          field: "price",
+          headerName: 'Price',
+          field: 'price',
           suppressSorting: true,
-          cellRenderer: "squareRenderer"
-        }
+          cellRenderer: 'squareRenderer',
+        },
       ],
       rowData: [
-        { make: "Toyota", model: "Celica", price: 35000 },
-        { make: "Ford", model: "Mondeo", price: 32000 },
-        { make: "Porsche", model: "Boxter", price: 72000 }
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 },
       ],
       context: { componentParent: this },
       frameworkComponents: {
-        squareRenderer: SquareRenderer
-      }
-    };
+        squareRenderer: SquareRenderer,
+      },
+    }
   }
 
   generateData(numRows) {
-    const data = [];
+    const data = []
     for (let i = 0; i < numRows; ++i) {
       data.push({
         make: carMakers[i % 7],
-        model: "CC" + i + 10,
-        price: i + 1251
-      });
+        model: 'CC' + i + 10,
+        price: i + 1251,
+      })
     }
 
-    return data;
+    return data
   }
 
   componentDidMount() {
@@ -74,7 +121,7 @@ class App extends Component {
     //   .then(result => result.json())
     //   .then(rowData => this.setState({ rowData }));
 
-    this.setState({ rowData: this.generateData(10000) });
+    this.setState({ rowData: this.generateData(10000) })
   }
 
   onButtonClick = e => {
@@ -86,43 +133,64 @@ class App extends Component {
     // alert(`Selected nodes: ${selectedDataStringPresentation}`);
 
     this.gridApi.forEachNode(node => {
-      if (node.data.make === "Ford") {
-        node.setSelected(true);
+      if (node.data.make === 'Ford') {
+        node.setSelected(true)
       }
-    });
-  };
+    })
+  }
 
   onCellClicked = e => {
-    console.log(e);
-  };
+    console.log(e)
+  }
 
   render() {
     return (
-      <div
-        className="ag-theme-balham"
-        style={{
-          height: "500px",
-          width: "100%"
-        }}
-      >
-        <h1>ag-Grid React</h1>
-        <button onClick={this.onButtonClick}>Select Ford rows</button>
-        <AgGridReact
-          enableSorting={true}
-          enableFilter={true}
-          rowSelection="multiple"
-          columnDefs={this.state.columnDefs}
-          enableColResize={true}
-          enableRangeSelection
-          suppressRowClickSelection
-          rowData={this.state.rowData}
-          onCellClicked={this.onCellClicked}
-          onGridReady={params => (this.gridApi = params.api)}
-          frameworkComponents={this.state.frameworkComponents}
-        />
-      </div>
-    );
+      <StyledDiv>
+        <AppHeader>
+          <h1>ag-Grid React</h1>
+        </AppHeader>
+
+        <AppBody>
+          <div
+            className="ag-theme-balham"
+            style={{
+              height: '500px',
+              width: '100%',
+            }}
+          >
+            <button onClick={this.onButtonClick}>Select Ford rows</button>
+            <AgGridReact
+              enableSorting={true}
+              enableFilter={true}
+              rowSelection="multiple"
+              columnDefs={this.state.columnDefs}
+              enableColResize={true}
+              enableRangeSelection
+              suppressRowClickSelection
+              rowData={this.state.rowData}
+              onCellClicked={this.onCellClicked}
+              onGridReady={params => (this.gridApi = params.api)}
+              frameworkComponents={this.state.frameworkComponents}
+            />
+          </div>
+        </AppBody>
+
+        {/* <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Modal</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            TEST
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button>Close</Button>
+          </Modal.Footer>
+        </Modal.Dialog> */}
+      </StyledDiv>
+    )
   }
 }
 
-export default App;
+export default App
